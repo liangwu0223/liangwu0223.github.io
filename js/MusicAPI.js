e0e4531e657b6e7b04db666b2080e4af16ee0b45
liangwu0223 <liangwu0223@gmail.com>
2024-05-20 13:08:44 -0500

    Digital Chalkboard nac bar

--- js/MusicAPI.js
+++ js/MusicAPI.js
@@ -1,8 +1,34 @@
 // MusicBrainz API: an interface to Music Brainz Database
 
-function searchArtist(){}
+function searchArtist(){
+    // Preprocess input + error handling
+    const searchInput = document.getElementById('artistname').value.trim()
+    if (searchInput === '') {
+        alert('Please enter an artist name');
+        return;
+    }
+    // create URL
+    const baseURL = 'https://musicbrainz.org/ws/2/artist/?query=';
+    const format = '&fmt=json';
+    const searchURL = baseURL + encodeURIComponent(searchInput) + format;
+    
+    // pass the input to the next function 
+    fetch(searchURL)
+    .then(response => response.json())
+    .then(data => {
+        displayArtists(data.artists);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+    
+}
 
-function displayArtists(){}
+
+function displayArtists(artist){
+
+
+}
 
 function fetchAlbums(){}
 
